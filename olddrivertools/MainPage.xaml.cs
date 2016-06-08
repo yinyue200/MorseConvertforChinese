7a09db31a5696c01ee1370f4a8b5a9dc5c2ee348
yinyue <yinyue200@outlook.com>
2016-06-08 16:05:27 +0800

    start

--- olddrivertools/MainPage.xaml.cs
+++ olddrivertools/MainPage.xaml.cs
@@ -26,5 +26,137 @@ namespace olddrivertools
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (checkbox.IsChecked == true)
+                {
+                    byte[] content = null;
+                    switch (jismichoose.SelectedIndex)
+                    {
+                        case 0:
+                            NewMethod2(out content);
+                            break;
+                        case 1:
+                            NewMethod16(out content);
+                            break;
+                        case 2:
+                            content = Convert.FromBase64String(inputbox.Text);
+                            return;
+                        case 3:
+                            outputbox.Text = MorseConvert.morse2word(inputbox.Text);
+                            return;
+                        case 4:
+                            outputbox.Text = MorseConvertChinese.morse2word(inputbox.Text);
+                            return;
+                    }
+                    switch (encodingchoose.SelectedIndex)
+                    {
+                        case 0:
+                            outputbox.Text = System.Text.Encoding.UTF8.GetString(content);
+                            break;
+                        case 1:
+                            outputbox.Text = System.Text.Encoding.Unicode.GetString(content);
+                            break;
+                        case 2:
+                            outputbox.Text = System.Text.Encoding.BigEndianUnicode.GetString(content);
+                            break;
+                    }
+                }
+                else
+                {
+                    byte[] content = null;
+                    switch (encodingchoose.SelectedIndex)
+                    {
+                        case 0:
+                            content = System.Text.Encoding.UTF8.GetBytes(inputbox.Text);
+                            break;
+                        case 1:
+                            content = System.Text.Encoding.Unicode.GetBytes(inputbox.Text);
+                            break;
+                        case 2:
+                            content = System.Text.Encoding.BigEndianUnicode.GetBytes(inputbox.Text);
+                            break;
+                    }
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    switch (jismichoose.SelectedIndex)
+                    {
+                        case 0:
+                            foreach (var one in content)
+                            {
+                                sb.Append(Convert.ToString(one, 2).PadLeft(8, '0'));
+                            }
+                            break;
+                        case 1:
+                            foreach (var one in content)
+                            {
+                                sb.Append(one.ToString("X2"));
+                            }
+                            break;
+                        case 2:
+                            outputbox.Text = Convert.ToBase64String(content);
+                            return;
+                        case 3:
+                            outputbox.Text = MorseConvert.word2morse(inputbox.Text);
+                            return;
+                        case 4:
+                            outputbox.Text = MorseConvertChinese.word2morse(inputbox.Text);
+                            return;
+                    }
+                    outputbox.Text = sb.ToString();
+                }
+            }
+            catch
+            {
+                outputbox.Text = "转换出错";
+            }
+           
+        }
+
+        private void NewMethod2(out byte[] content)
+        {
+            int i, alli;
+            char[] list;
+            i = 0;
+            alli = 0;
+            list = new char[8];
+            content = new byte[inputbox.Text.Length / list.Length];
+            foreach (var one in inputbox.Text)
+            {
+                list[i] = one;
+                i++;
+                if (i >= list.Length)
+                {
+                    content[alli] = Convert.ToByte(new string(list), 2);
+                    alli++;
+                    i = 0;
+                }
+            }
+        }
+        private void NewMethod16(out byte[] content)
+        {
+            if ((inputbox.Text.Length % 2) != 0)
+                throw new ArgumentException("十六进制字符串长度不对");
+            content = new byte[inputbox.Text.Length / 2];
+            for (int i = 0; i < content.Length; i++)
+            {
+                content[i] = Convert.ToByte(inputbox.Text.Substring(i * 2, 2).Trim(), 0x10);
+            }
+        }
+        private void jismichoose_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch(jismichoose.SelectedIndex)
+            {
+                case 4:
+                case 5:
+                    encodingchoose.Visibility = Visibility.Collapsed;
+                    break;
+                default:
+                    encodingchoose.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
     }
 }
